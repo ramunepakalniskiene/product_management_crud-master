53cde4e4ce8b15aeb1828bdd584cf0f8acdbfab6
Ramune <ramune.pakalniskiene@gmail.com>
2023-10-02 10:16:13 +0300

    Sort button works. 10.02

--- Products/src/main/java/com/example/products/ProductController.java
+++ Products/src/main/java/com/example/products/ProductController.java
@@ -3,10 +3,9 @@ package com.example.products;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
 @Controller
@@ -136,5 +135,17 @@ public class ProductController {
         model.addAttribute("search", productService.findProductByName(name));
         return "filters";
     }
+//    //*************************************************************************
+//    @RequestMapping(value = "/sell-product/{id}", method = RequestMethod.POST)
+//    public String handle(@ModelAttribute ("sellProduct") Product product, BindingResult result, RedirectAttributes redirectAttrs) {
+//        if (result.hasErrors()) {
+//            redirectAttrs.addFlashAttribute("error", "The error XYZ occurred.");
+//            return "redirect:/products/";
+//
+//        } else
+//        // Save account ...
+//        redirectAttrs.addAttribute("id", product.getId()).addFlashAttribute("success", "Product sold!");
+//        return "redirect:/addProduct";
+//    }
 
 }
